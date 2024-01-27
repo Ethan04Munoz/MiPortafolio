6c37eb044656efb11e78da3d5f01c404ca4d0b98
Your Name <you@example.com>
2024-01-27 16:55:14 -0600

    Router en la App y separación de los proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import { useState } from 'react'
-import './App.css'
-import Flipcard from './componentes/Flipcard'
+import {HashRouter,Routes,Route} from "react-router-dom";
+import Proyectos from './Proyectos';
 
 function App() {
   return (
     <>
-    <div className="grid-container">
-    <Flipcard 
-        imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
-        cardTitle="Gato"
-        cardContent="Gato insano que hace wasa"
-        cardLinkRoute="https://facebook.com"
-        cardLinkText="Ver gato"
-      />
-            <Flipcard 
-        imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
-        cardTitle="Gato"
-        cardContent="Gato insano que hace wasa"
-        cardLinkRoute="https://facebook.com"
-        cardLinkText="Ver gato"
-      /> 
-    </div>
-
+      <HashRouter>
+      <Routes>
+        <Route exact path="/proyectos" element={<Proyectos/>}/>
+        <Route exact path="/" element={<Proyectos/>}/>
+      </Routes>
+      </HashRouter>
     </>
   )
 }
