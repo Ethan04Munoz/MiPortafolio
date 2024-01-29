120895a52fbf5a5ec3d27b6152ab573c630cf6c0
Your Name <you@example.com>
2024-01-28 21:18:32 -0600

    Mover la barra lateral a componentes

---
+++ src/componentes/BarraLateral.jsx
@@ -0,0 +1,33 @@
+import './LetraE.css';
+import './BarraLateral.css';
+import Proyectos from '../Proyectos';
+function BarraLateral(){
+    return (
+        <div className="BarraLateral">
+            <div class="letraE3D">E</div>
+            <h1>Ethan Muñoz - Desarrollador Web</h1>
+            <a href="/#/sobremi">
+                <div>
+                Sobre mí
+                </div>
+            </a>
+            <a href="/#/proyectos">
+                <div>
+                Proyectos
+                </div>
+            </a>
+            <a href="/#/tecnologias">
+                <div>
+                Tecnologías
+                </div>
+            </a>
+            <a href="/#/curriculum">
+                <div>
+                Curriculum
+                </div>
+            </a>
+        </div>
+    )
+}
+
+export default BarraLateral;
