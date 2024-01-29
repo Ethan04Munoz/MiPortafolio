5536b5be59fdadd3557d1b2a7605f405a2e6b2c7
Your Name <you@example.com>
2024-01-28 20:51:18 -0600

    Añadido de rutas tecnologias y sobre mi

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import {HashRouter,Routes,Route} from "react-router-dom";
 import Proyectos from './Proyectos';
+import BarraLateral from './BarraLateral';
+import Tecnologias from './Tecnologías';
+import SobreMi from './Sobre mi';
 
 function App() {
   return (
     <>
       <HashRouter>
       <Routes>
-        <Route exact path="/proyectos" element={<Proyectos/>}/>
-        <Route exact path="/" element={<Proyectos/>}/>
+        <Route path="/proyectos" element={<Proyectos/>}/>
+        <Route path='/tecnologias' element={<Tecnologias/>}/>
+        <Route path="/sobremi" element={<SobreMi/>}/>
+        <Route path='/' element={<SobreMi/>}/>
       </Routes>
       </HashRouter>
     </>
