60421a3ac957a12988587d6d4593b14cfb12775b
Your Name <you@example.com>
2024-01-28 20:51:48 -0600

    rutas en la barra lateral

--- src/BarraLateral.jsx
+++ src/BarraLateral.jsx
@@ -6,17 +6,17 @@ function BarraLateral(){
         <div className="BarraLateral">
             <div class="letraE3D">E</div>
             <h1>Ethan Muñoz - Desarrollador Web</h1>
-            <a href="">
+            <a href="/#/proyectos">
                 <div>
                 Proyectos
                 </div>
             </a>
-            <a href="">
+            <a href="/#/tecnologias">
                 <div>
                 Tecnologías
                 </div>
             </a>
-            <a href="">
+            <a href="/#/sobremi">
                 <div>
                 Sobre mí
                 </div>
