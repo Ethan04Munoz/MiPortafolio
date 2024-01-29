f8f833cf46ea5a0fa57fe15478dbba71c50dff47
Your Name <you@example.com>
2024-01-28 20:53:21 -0600

    Creación de componente e página Tecnologías y sobre mi

---
+++ src/Sobre mi.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import BarraLateral from './BarraLateral';
+import Flipcard from './componentes/Flipcard';
+
+function SobreMi() {
+    return (
+        <div className='grid-BarraCuerpo'>
+            <BarraLateral/>
+            <div>
+                <h1>Desarrollador web, fanatico del buscaminas.</h1>
+                <div class="icon-bar">
+                    <a href="https://github.com/tu-usuario" target="_blank" class="icon-link">
+                        <img src="../src/img/github-Dark.svg" alt="GitHub" />
+                    </a>
+                    <a href="path-to-your-cv.pdf" target="_blank" class="icon-link">
+                        <img src="../src/img/cv-Dark.svg" alt="CV" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/tu-usuario" target="_blank" class="icon-link">
+                        <img src="../src/img/linkedin-Dark.svg" alt="LinkedIn" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SobreMi;
+
---
+++ src/Tecnologías.jsx
@@ -0,0 +1,84 @@
+import './App.css'
+import BarraLateral from './BarraLateral';
+import Flipcard from './componentes/Flipcard';
+
+function Tecnologias() {
+    return (
+        <div className='grid-BarraCuerpo'>
+            <BarraLateral/>
+            <div>
+            <h1>Frontend</h1>
+            <div className="grid-container">
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="HTML"
+                    cardContent="Utilicé HTML en todos los proyectos que involucran React. "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="CSS"
+                    cardContent="Utilice CSS para dar estilos y realizar animaciones en los proyectos de "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="React"
+                    cardContent="Utilice Javascript vanilla y herramientas propias del mismo como LocalStorage en "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="React"
+                    cardContent="Utilice React para los proyectos de "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+            </div>
+            <h1>Backend</h1>
+            <div className="grid-container">
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="Node JS"
+                    cardContent="Utilicé HTML en todos los proyectos que involucran React. "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="Express.js"
+                    cardContent="Utilicé HTML en todos los proyectos que involucran React. "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="MySQL"
+                    cardContent="Utilicé HTML en todos los proyectos que involucran React. "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="Stripe"
+                    cardContent="Utilicé HTML en todos los proyectos que involucran React. "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                <Flipcard 
+                    imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                    cardTitle="APIs"
+                    cardContent="Utilicé HTML en todos los proyectos que involucran React. "
+                    cardLinkRoute="https://facebook.com"
+                    cardLinkText="Visitar"
+                />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tecnologias
