7d7b4d3ae3dc29b6e7ba2c88ee5e17f8e6950294
Your Name <you@example.com>
2024-01-31 15:44:58 -0600

    Creación del componente de la barra inferior

---
+++ src/componentes/BarraInferior.jsx
@@ -0,0 +1,20 @@
+import './BarraInferior.css';
+function BarraInferior(){
+    return (
+        <div className="BarraInferior">
+            <div>
+                <a href="https://github.com/tu-usuario" target="_blank" class="icon-link">
+                    <img src="../src/img/github-Dark.svg" alt="GitHub" />
+                </a>
+            </div>
+            <a href="path-to-your-cv.pdf" target="_blank" class="icon-link">
+                <img src="../src/img/cv-Dark.svg" alt="CV" />
+            </a>
+            <a href="https://www.linkedin.com/in/tu-usuario" target="_blank" class="icon-link">
+                <img src="../src/img/linkedin-Dark.svg" alt="LinkedIn" />
+            </a>
+        </div>
+    )
+}
+
+export default BarraInferior;
