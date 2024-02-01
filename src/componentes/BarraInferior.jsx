c1ffe7da082e3f080afc96e04ce651bd995bec7f
Your Name <you@example.com>
2024-01-31 21:19:44 -0600

    La barra inferior cambia la bandera al hacerle clic

--- src/componentes/BarraInferior.jsx
+++ src/componentes/BarraInferior.jsx
@@ -1,5 +1,16 @@
 import './BarraInferior.css';
+import { useState } from 'react';
+
+let espanolImg = '../src/img/español-Rounded.png';
+let inglesImg = '../src/img/ingles-Rounded.svg';
 function BarraInferior(){
+    // Inicializar el estado con una de las imágenes
+    const [imagenActual, setImagenActual] = useState(espanolImg);
+
+    // Función para cambiar la imagen
+    const cambiarIdioma = () => {
+        setImagenActual(imagenActual === espanolImg ? inglesImg : espanolImg);
+    };
     return (
         <div className="BarraInferior">
             <div>
@@ -8,14 +19,19 @@ function BarraInferior(){
                 </a>
             </div>
             <div>
-            <a href="path-to-your-cv.pdf" target="_blank" class="icon-link">
-                <img src="../src/img/cv-Dark.svg" alt="CV" />
-            </a>
+                <a href="path-to-your-cv.pdf" target="_blank" class="icon-link">
+                    <img src="../src/img/cv-Dark.svg" alt="CV" />
+                </a>
             </div>
             <div>
-            <a href="https://www.linkedin.com/in/tu-usuario" target="_blank" class="icon-link">
-                <img src="../src/img/linkedin-Dark.svg" alt="LinkedIn" />
-            </a>
+                <a href="https://www.linkedin.com/in/tu-usuario" target="_blank" class="icon-link">
+                    <img src="../src/img/linkedin-Dark.svg" alt="LinkedIn" />
+                </a>
+            </div>
+            <div>
+                <a class="icon-link" onClick={cambiarIdioma}>
+                    <img src={imagenActual} alt="Idioma" className='imgIdioma'/>
+                </a>
             </div>
         </div>
     )
