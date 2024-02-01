062f2dd110c9642c3b429801698226f94fb05f4c
Your Name <you@example.com>
2024-01-31 20:41:53 -0600

    Corregir tamaño de los enlaces en la barra inferior

--- src/componentes/BarraInferior.jsx
+++ src/componentes/BarraInferior.jsx
@@ -7,12 +7,16 @@ function BarraInferior(){
                     <img src="../src/img/github-Dark.svg" alt="GitHub" />
                 </a>
             </div>
+            <div>
             <a href="path-to-your-cv.pdf" target="_blank" class="icon-link">
                 <img src="../src/img/cv-Dark.svg" alt="CV" />
             </a>
+            </div>
+            <div>
             <a href="https://www.linkedin.com/in/tu-usuario" target="_blank" class="icon-link">
                 <img src="../src/img/linkedin-Dark.svg" alt="LinkedIn" />
             </a>
+            </div>
         </div>
     )
 }
