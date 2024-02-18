fae30160cba061af6b298577f1c1000f26f25384
Your Name <you@example.com>
2024-02-18 13:48:14 -0600

    Comentados los proyectos que no estan listos

--- src/Proyectos.jsx
+++ src/Proyectos.jsx
@@ -25,7 +25,7 @@ function Proyectos() {
                 cardLinkRoute="https://facebook.com"
                 cardLinkText="Visitar"
             /> 
-            <Flipcard 
+            {/*<Flipcard 
                 imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
                 cardTitle="Password Generator"
                 cardContent="Utilizando React y CSS3,
@@ -62,21 +62,12 @@ function Proyectos() {
             /> 
             <Flipcard 
                 imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
-                cardTitle="Tienda online"
-                cardContent="Utilizando la API de TMDB, React, CSS3 y LocalStorage
-                    desarrollé un juego de Quiz de cine donde los usuarios puedan poner a prueba sus conocimientos sobre películas, 
-                    actores, directores, etc. Utilizo la API para generar preguntas y respuestas."
-                cardLinkRoute="https://facebook.com"
-                cardLinkText="Visitar"
-            /> 
-            <Flipcard 
-                imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
                 cardTitle="Recetario"
                 cardContent="Utilizando la React, CSS3 y APIs desarrolle una herramienta que permite a los 
                     usuarios ingresar ingredientes que tienen en casa y obtener posibles recetas que pueden elaborar."
                 cardLinkRoute="https://facebook.com"
                 cardLinkText="Visitar"
-            /> 
+  /> 
             <Flipcard 
                 imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
                 cardTitle="Expense tracker"
@@ -89,12 +80,21 @@ function Proyectos() {
             <Flipcard 
                 imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
                 cardTitle="Feliz cumpleaños!"
-                cardContent="Utilizando la React y CSS3 desarrollé una página web donde con cada toque en un pastel se añade una vela.
+                cardContent="Utilizando React y CSS3 desarrollé una página web donde con cada toque en un pastel se añade una vela.
                     Para disminuir la cantidad de velas, tienes que soplar las velas. Finalmente, con un botón muestra un mensaje felicitandote
                      por tus x años."
                 cardLinkRoute="https://facebook.com"
                 cardLinkText="Visitar"
             /> 
+            <Flipcard 
+                imgUrl="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1600&h=1067"
+                cardTitle="Spinners de carga"
+                cardContent="Utilizando React y CSS3 desarrollé un sitio web que permite crear indicadores de carga personalizados,
+                    permitiendo configurar desde los colores, opacidad, velocidad, ancho, entre muchas otras cosas, y darte el código ya sea en código JSX o en
+                    código html + css."
+                cardLinkRoute="https://facebook.com"
+                cardLinkText="Visitar"
+            /> */}
         </div>
         <BarraInferior/>
     </div>
